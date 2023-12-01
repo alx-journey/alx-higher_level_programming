98ffef713d23a1692d7a41936c49a165d2d12385
macbook <user@macbooks-MacBook-Pro.local>
2023-12-01 01:42:07 +0100

    solve python challenges

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
+
 def magic_calculation(a, b):
     """Match bytecode provided by Holberton School."""
- from magic_calculation_102 import add, sub
+    from magic_calculation_102 import add, sub
 
     if a < b:
         c = add(a, b)
@@ -12,3 +13,4 @@ def magic_calculation(a, b):
 
     else:
         return(sub(a, b))
+
